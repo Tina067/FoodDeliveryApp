c0f124c9a1bc775ad4c951b5a685bf372de5f8fd
tina067 <tinamurmu222@gmail.com>
2024-06-10 20:25:21 +0530

    lets get classy part3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import User from './User';
 import UserClass from './UserClass';
 
 const About = () => {
@@ -7,7 +6,6 @@ const About = () => {
     <div>
         <h1>About</h1>
         <h2>This is Namaste React Web Series.</h2>
-        <User name={"Tina Murmu (function)"}/>
         <UserClass  name = {"Tina Murmu (class)"} location={"Gwalior(class)"}/>
     </div>
 
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 
 const User = (props) => {
+    const [count,] = useState(0);
+    const [count2] = useState(2);
+
     return(
         <div className='user-card'>
+            <h1>Count : {count}</h1>
+            <h1>Count2 : {count2}</h1>
             <h2>Name: {props.name}</h2>
             <h3>Location: Gwalior</h3>
             <h4>Contact: @tinaa_1</h4>
